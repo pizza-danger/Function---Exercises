b6f14bb0407c3233fc7fa16be47cb0db35b89650
thereyner <33945338+pizza-danger@users.noreply.github.com>
2021-06-15 14:37:01 +0000

    c. Rectangle

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 function makeLine(size) {
   //let hash = "#";
   let line = "";
-  for (let i = 0; i <= size; i++) {
+  for (let i = 0; i <= 4; i++) {
     //console.log(hash);
     line += "#";
   }
   return line;
 }
+
 //console.log(makeLine(5));
 
-function makeSquare(size) {
-  let line = makeLine(5);
+/*function makeSquare(size) {
+  let line = makeLine();
   let square = "";
   for (let i = 0; i <= size; i++) {
-  square += line + '\n';
+  square += line;
 }
 return square;
 }
 console.log(makeSquare(4));
+*/
+
+function makeRectangle(width, height) {
+  let rectangle = "";
+  for(let i = 0; i < height; i++) {
+    rectangle += makeLine(width) + '\n';
+  }
+  return rectangle.slice(0, -1);
+}
+console.log(makeRectangle(5,3));
