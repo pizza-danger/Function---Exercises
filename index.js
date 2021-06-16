f974ca214868612b1b295908d689da45bd4b3b6e
thereyner <33945338+pizza-danger@users.noreply.github.com>
2021-06-16 13:37:27 +0000

    Still working...

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 //a.
 function makeLine(size) {
-  //let hash = "#";
   let line = "";
   for (let i = 0; i < size; i++) {
-    //console.log(hash);
     line += "#";
   }
   return line;
@@ -12,36 +10,69 @@ function makeLine(size) {
 //console.log(makeLine(5));
 
 //b.
-function makeSquare(size) {
-  let line = makeLine();
+/*function makeSquare(size) {
+  let line = makeLine(size);
   let square = "";
-  for (let i = 0; i <= size; i++) {
+  for (let i = 0; i < size; i++) {
   square += line + '\n';
 }
-return square;
-}
-//console.log(makeSquare(4));
+return square.slice(0,-1);
+}*/
+//console.log(makeSquare(5));
 
 //c.
 function makeRectangle(width, height) {
   let rectangle = "";
   for(let i = 0; i < height; i++) {
-    rectangle += makeLine(width);
-    console.log(makeLine(width));
+    rectangle += makeLine(width) + '\n';
   }
   return rectangle.slice(0,-1);
 }
 //console.log(makeRectangle(5,3));
 
-//d.
+//d. QUESTION - Ask for TA assistance
+function makeSquare(size) {
+  let square = makeRectangle(size, size);
+  }
+
+//console.log(makeSquare(5));
+
+//e.
 function makeDownwardStairs(height) {
   let stairs = "";
   for(let i = 0; i < height; i++) {
     stairs += makeLine(i + 1) + '\n';
-    //console.log(makeLine(i));
   }
   return stairs.slice(0,-1);
 }
 
-console.log(makeDownwardStairs(5))
+//console.log(makeDownwardStairs(5))
 
+//f.
+function makeSpaces(numSpaces) {
+  let spaces = "";
+  for (let i = 0; i < numSpaces; i++) {
+    spaces += " ";
+  }
+  return spaces;
+}
+//console.log(makeSpaces(5));
+
+function makeChars(numChars) {
+  let chars = "";
+  for (let i = 0; i < numChars; i++) {
+    chars += "#";
+  }
+  return chars;
+}
+//console.log(makeChars(5));
+
+function makeSpaceLine(numSpaces, numChars) {
+  let spaceLine = "";
+  for(let i = 0; i < makeSpaceLine.length; i++) {
+     spaceLine += makeSpaces(numSpaces) + makeChars(numChars) + makeSpaces(numSpaces);
+  }
+  return spaceLine;
+}
+
+console.log(makeSpaceLine(3,5));
